c0b6128b6047bf6fa7b790cc82e0c6044af4ab6d
cherrydub <cherrydub@gmail.com>
2024-10-27 13:12:19 +0000

    fix: rename UseSonner

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,12 @@
-import InitiateSonner from '@/components/UseSonner'
 import { ThemeToggle } from '@/components/ThemeToggle'
+import UseSonner from '@/components/UseSonner'
 
 export default function Home () {
   return (
     <div className='flex flex-col items-center justify-center h-screen gap-4'>
       Hello World
       <ThemeToggle />
-      <InitiateSonner message='Hello World' />
+      <UseSonner message='Hello World' />
     </div>
   )
 }
--- components/UseSonner.tsx
+++ components/UseSonner.tsx
@@ -4,7 +4,7 @@ import React from 'react'
 import { toast } from 'sonner'
 import { Button } from './ui/button'
 
-export default function InitiateSonner ({ message }: { message: string }) {
+export default function UseSonner ({ message }: { message: string }) {
   return (
     <Button onClick={() => toast(message || 'Hello World')}>toast 🍻</Button>
   )
